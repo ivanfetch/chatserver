47349c5e805d6a07e81447f9c752c8258e9a2056
Ivan Fetch <ivan@ivanfetch.com>
2023-06-29 19:26:58 -0600

    Finish connection-manager cleanup then exit, type connection implements Read and Write methods to avoid errors for closed connections, failed attempt at seasing to accept connections while cleaning up / exiting

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 // connection holds information about a connection to the chat server.
 type connection struct {
 	netConn  net.Conn // TCP connection
+	isClosed bool     // if netConn has been closed by us
 	nickname string   // optional nickname, accompanying chat messages
 }
 
@@ -26,19 +27,44 @@ func newConnection(netConn net.Conn) *connection {
 }
 
 // Close wraps the Close method of the member net.Conn type.
-func (c connection) Close() {
+func (c *connection) Close() {
 	debugLog.Printf("closing connection %s", c.UniqueID())
+	c.isClosed = true
 	c.netConn.Close()
+}
+
+// Write wraps netCOnn.Write, only writing if isClosed == false
+func (c *connection) Write(b []byte) (int, error) {
+	if c.isClosed == true {
+		debugLog.Printf("not writing to this closed connection %s: %s\n", c.UniqueID(), string(b))
+		return 0, nil
+	}
+	return c.netConn.Write(b)
+}
+
+// Read wraps netCOnn.Read, only reading if isClosed == false
+func (c *connection) Read(b []byte) (int, error) {
+	if c.isClosed == true {
+		debugLog.Printf("not reading from this closed connection %s\n", c.UniqueID())
+		return 0, nil
+	}
+	return c.netConn.Read(b)
 }
 
 // uniqueID returns a unique identifier for the chat connection,
 // differentiating connections by using the source IP address and port, and
 // an optional nickname.
 func (c connection) UniqueID() string {
-	if c.nickname != "" {
-		return fmt.Sprintf("%s-%s", c.nickname, c.netConn.RemoteAddr().String())
+	var remoteAddr string
+	if c.netConn == nil {
+		remoteAddr = "never connected"
+	} else {
+		remoteAddr = c.netConn.RemoteAddr().String()
 	}
-	return c.netConn.RemoteAddr().String()
+	if c.nickname != "" {
+		return fmt.Sprintf("%s-%s", c.nickname, remoteAddr)
+	}
+	return remoteAddr
 }
 
 // Nickname returns the nickname for the chat connection if it has been set,
@@ -65,7 +91,7 @@ var debugLog *log.Logger = log.New()
 
 // startConnectionAndMessageManager runs a goroutine that tracks connections to the chat
 // server, and processes messages submitted by connections.
-func startConnectionAndMessageManager(stopCh chan os.Signal, addConnCh, removeConnCh chan *connection, addMessageCh chan message) {
+func startConnectionAndMessageManager(listener net.Listener, stopCh <-chan struct{}, addConnCh, removeConnCh chan *connection, addMessageCh chan message) {
 	var currentConnections []*connection
 	var cleaningUp bool // Indicates goroutines are in the process of cleaning up, to exit
 	go func() {
@@ -76,26 +102,32 @@ func startConnectionAndMessageManager(stopCh chan os.Signal, addConnCh, removeCo
 				debugLog.Printf("adding connection from %s", newConn.UniqueID())
 				currentConnections = append(currentConnections, newConn)
 			case removeConn := <-removeConnCh:
-				debugLog.Printf("removing connection %s", removeConn.UniqueID)
-				newConnections := make([]*connection, len(currentConnections)-1)
-				newI := 0
-				for _, conn := range currentConnections {
-					if conn.UniqueID() != removeConn.UniqueID() {
-						newConnections[newI] = conn
-						newI++
+				if removeConn != nil {
+					debugLog.Printf("removing connection %s", removeConn.UniqueID())
+					newConnections := make([]*connection, len(currentConnections)-1)
+					newI := 0
+					for _, conn := range currentConnections {
+						if conn.UniqueID() != removeConn.UniqueID() {
+							newConnections[newI] = conn
+							newI++
+						}
 					}
+					currentConnections = newConnections
+					removeConn.Close()
 				}
-				currentConnections = newConnections
 			case newMessage := <-addMessageCh:
 				debugLog.Printf("processing new message from %s: %s", newMessage.connection.Nickname(), newMessage.text)
-				cleaningUp = true
 				go broadcast(newMessage, currentConnections, removeConnCh, false)
-			case stopSig := <-stopCh:
-				debugLog.Printf("received signal %v, cleaning up and exiting", stopSig)
-				go broadcast(message{
-					text:       "You are being disconnected because the chat-server is exiting. So long...",
-					connection: nil, // no connection because this is a system message
-				}, currentConnections, removeConnCh, true)
+			case <-stopCh:
+				if !cleaningUp {
+					debugLog.Printf("the connection manager is starting clean up")
+					cleaningUp = true
+					listener.Close()
+					go broadcast(message{
+						text:       "You are being disconnected because the chat-server is exiting. So long...",
+						connection: &connection{nickname: "system"},
+					}, currentConnections, removeConnCh, true)
+				}
 			default:
 				// Avoid blocking thecontaining loop
 			}
@@ -103,22 +135,22 @@ func startConnectionAndMessageManager(stopCh chan os.Signal, addConnCh, removeCo
 				break
 			}
 		}
-		debugLog.Println("connection manager is finished")
 		if cleaningUp {
-			debugLog.Printf("cleanup complete")
+			debugLog.Printf("the connection manager has finished cleaning up")
 		}
+		debugLog.Println("connection manager exiting")
 	}()
 }
 
 // processInput scans a chat connection for text and hands chat commands or
 // messages.
 func processInput(con *connection, addMessageCh chan message, removeConnCh chan *connection) {
-	log.Println("saying hello to our connection")
-	fmt.Fprintln(con.netConn, `Well hello there!
+	debugLog.Printf("saying hello then reading input from connection %s", con.UniqueID())
+	fmt.Fprintln(con, `Well hello there!
 
 Anything you type to me will be displayed in the output of this chat-server.
 `)
-	scanner := bufio.NewScanner(con.netConn)
+	scanner := bufio.NewScanner(con)
 	for scanner.Scan() {
 		line := scanner.Text()
 		debugLog.Printf("received from %s: %s", con.UniqueID(), line)
@@ -126,7 +158,6 @@ Anything you type to me will be displayed in the output of this chat-server.
 			exiting := processCommands(line, con)
 			if exiting {
 				removeConnCh <- con
-				con.Close()
 				return
 			}
 			continue
@@ -137,9 +168,10 @@ Anything you type to me will be displayed in the output of this chat-server.
 		}
 	}
 	err := scanner.Err()
-	if err != nil {
-		debugLog.Println("while reading from %s: %v", con.UniqueID(), err)
+	if !con.isClosed && err != nil {
+		debugLog.Printf("while reading from %s: %v", con.UniqueID(), err)
 	}
+	debugLog.Printf("reading input is exiting for connection %s", con.UniqueID())
 }
 
 func broadcast(msg message, allConnections []*connection, removeConnCh chan *connection, disconnect bool) {
@@ -150,14 +182,12 @@ func broadcast(msg message, allConnections []*connection, removeConnCh chan *con
 	}
 	for _, con := range allConnections {
 		var sender string
-		if msg.connection == nil {
-			sender = "system:"
-		} else if msg.connection != nil && con.UniqueID() == msg.connection.UniqueID() {
+		if msg.connection != nil && con.UniqueID() == msg.connection.UniqueID() {
 			sender = ">" // this recipient is the message-sender
 		} else {
 			sender = fmt.Sprintf("%s:", msg.connection.nickname)
 		}
-		_, err := fmt.Fprintf(con.netConn, "%s %s\n", sender, msg.text)
+		_, err := fmt.Fprintf(con, "%s %s\n", sender, msg.text)
 		if err != nil {
 			debugLog.Printf("error writing to %v: %v", con.netConn, err)
 			removeConnCh <- con
@@ -174,7 +204,7 @@ func processCommands(input string, con *connection) (clientIsLeaving bool) {
 	fields := strings.Fields(input)
 	switch strings.ToLower(fields[0][1:]) { // first word minus its first character(/)
 	case "quit", "exit", "leave":
-		fmt.Fprintf(con.netConn, "You're leaving? Ok - have a nice day. :)\n")
+		fmt.Fprintf(con, "You're leaving? Ok - have a nice day. :)\n")
 		debugLog.Printf("client %s has signed off", con.UniqueID())
 		return true
 	case "nickname", "nick":
@@ -183,12 +213,12 @@ func processCommands(input string, con *connection) (clientIsLeaving bool) {
 			con.nickname = fields[1]
 		}
 	case "help":
-		fmt.Fprintf(con.netConn, `Available commands are:
+		fmt.Fprintf(con, `Available commands are:
 /quit|leave|exit - Sign off and disconnect from chat.
 /nick|nickname - Set your nickname, to be displayed with your messages.
 `)
 	default:
-		fmt.Fprintf(con.netConn, "%q is an invalid command, please use /help for a list of commands.\n", fields[0])
+		fmt.Fprintf(con, "%q is an invalid command, please use /help for a list of commands.\n", fields[0])
 	}
 	return false
 }
@@ -196,6 +226,19 @@ func processCommands(input string, con *connection) (clientIsLeaving bool) {
 // createSignalHandler returns a channel that will be closed when SIGTERM
 // and SIGINT signals are received.
 // This channel can then be used to trigger cleanup and exit.
+func createSignalHandler() (stopChannel <-chan struct{}) {
+	stop := make(chan struct{})
+	// Create another channel that receives SIGTERM and SIGINT signals and triggers cleanup and exit.
+	ch := make(chan os.Signal, 2)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-ch
+		debugLog.Printf("received signal %s, exiting...\n", sig)
+		close(stop)
+	}()
+	return stop
+}
+
 func main() {
 	debugLog.SetFormatter(&log.TextFormatter{
 		PadLevelText: true,
@@ -207,22 +250,28 @@ func main() {
 		debugLog.Fatalf("cannot listen: %v", err)
 	}
 	debugLog.Printf("listening for connections on %s", listenAddress)
-	stopCh := make(chan os.Signal, 2)
-	signal.Notify(stopCh, syscall.SIGINT, syscall.SIGTERM)
+	stopCh := createSignalHandler()
 	addConnCh := make(chan *connection)
 	removeConnCh := make(chan *connection)
 	addMessageCh := make(chan message)
-	startConnectionAndMessageManager(stopCh, addConnCh, removeConnCh, addMessageCh)
+	startConnectionAndMessageManager(l, stopCh, addConnCh, removeConnCh, addMessageCh)
 	debugLog.Println("waiting for new connections")
+continueToListen:
 	for {
-		netConn, err := l.Accept()
-		if err != nil {
-			debugLog.Printf("while accepting a connection: %v", err)
-			continue
+		select {
+		case <-stopCh:
+			debugLog.Println("no longer accepting connections")
+			break continueToListen
+		default:
+			netConn, err := l.Accept()
+			if err != nil {
+				debugLog.Printf("while accepting a connection: %v", err)
+				continue
+			}
+			debugLog.Printf("accepted connection from %s", netConn.RemoteAddr())
+			conn := newConnection(netConn)
+			addConnCh <- conn
+			go processInput(conn, addMessageCh, removeConnCh)
 		}
-		debugLog.Printf("accepted connection from %s", netConn.RemoteAddr())
-		conn := newConnection(netConn)
-		addConnCh <- conn
-		go processInput(conn, addMessageCh, removeConnCh)
 	}
 }
